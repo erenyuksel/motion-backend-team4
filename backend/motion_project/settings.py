be759c9b425e1a0e680948b31aa172f9d87d4a97
Toni Pinjuh <tonipinjuh89@gmail.com>
2024-04-04 00:12:31 +0200

    added secret key

--- backend/motion_project/settings.py
+++ backend/motion_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j1txkp^tjefxgel(1i3dd%9ri3mm8zqi#z06sa(rq!d$)e(08b'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
