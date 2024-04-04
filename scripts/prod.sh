1640d97c7c380f73c98024f70da3d3d81a86346e
Toni Pinjuh <tonipinjuh89@gmail.com>
2024-04-04 12:25:33 +0200

    added ngnix service

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -2,4 +2,4 @@
 python -c "import time; time.sleep(3)" # Wait 3 seconds for postgres service to boot up
 python manage.py collectstatic --no-input
 python manage.py migrate
-gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
+gunicorn -w 4 -b 0.0.0.0:8000 motion_project.wsgi:application
